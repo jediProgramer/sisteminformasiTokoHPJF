44f07415039476a5cca7a969a1d549e510dc6c5d
jediProgramer <erlangga.kmoekasan@gmail.com>
2022-06-02 19:34:49 +0700

    first commit

---
+++ src/main/java/com/erlanggstudio/sisteminformasitokohpjf/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.erlanggstudio.sisteminformasitokohpjf;
+
+import com.erlanggstudio.sisteminformasitokohpjf.view.MainForm;
+
+/**
+ *
+ * @author erlanga
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainForm mainForum = new MainForm();
+        mainForum.setVisible(true);
+    }
+}
